65f1188e357a1711afb66556c07b13dabd714278
elliotjhan <elliotjhan@gmail.com>
2022-06-01 14:53:20 -0700

    game questions

--- beer-basics/App.js
+++ beer-basics/App.js
@@ -18,7 +18,7 @@ const App = () => {
   return(
     <NavigationContainer>
       <Tab.Navigator 
-        initialRouteName='Loading'
+        initialRouteName='Game'
         screenOptions={{
           tabBarInactiveTintColor: 'gray',
           tabBarActiveTintColor: '#00a39b',
--- beer-basics/components/game.js
+++ beer-basics/components/game.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 import { useFonts } from 'expo-font';
 
-
 const Game = () => {
-  useFonts({
-    'Quicksand': require('./../assets/fonts/Quicksand-Regular.ttf'),
-  });
+  useFonts({ 'Quicksand': require('./../assets/fonts/Quicksand-Regular.ttf')});
+  const questionList = [
+    {
+      prompt: 'Which of these is not a primary ingredient of beer?',
+      choices: ['water', 'yeast', 'malt', 'sugar'],
+      answer: 3
+    },
+    {
+      prompt: 'True of False? Hops are required to make beer.',
+      choices: ['True', 'False'],
+      answer: 1
+    },
+    {
+      prompt: 'What country was responsible for making hops popular around the world?',
+      choices: ['USA', 'Germany', 'Belgium', 'China'],
+      answer: 1
+    }
+  ];
+  const [questionCount, setQuestionCount] = useState(0);
+  //useEffect(() => {});
+
   return (
-  <View>
-      <Text style={styles.titleText}>Placeholder for multiple choice quiz game</Text>
-  </View>
-  )
+    <View>
+      <Text style={styles.titleText}>Test Your Knowledge!</Text>
+      <Pressable
+        style={styles.startButton}
+        onPress={() => setQuestionCount(questionCount + 1)}
+      >
+        <Text style={styles.buttonText}>Start</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -19,8 +42,23 @@ const styles = StyleSheet.create({
     fontFamily: "Quicksand"
   },
   titleText: {
-    fontSize: 20,
-    fontFamily: "Quicksand"
+    fontSize: 40,
+    fontFamily: "Quicksand",
+    textAlign: 'center',
+    marginTop: '10vh'
+  },
+  startButton: {
+    marginHorizontal: 'auto',
+    marginTop: '5vh',
+    backgroundColor: '#00a39b',
+    padding: 15,
+    borderRadius: 7
+  },
+  buttonText: {
+    fontSize: 30,
+    color: 'white',
+    textAlign: 'center',
+
   }
 });
 
